d501a62c0a645e1b06da6ec7dd8e541a38f6af6f
Ong Andre <andre_28896@hotmail.com>
2020-06-11 19:24:17 +0800

    Use h instead of f when choosing cells This was causing issues as h was affected by a volatile g Also added some comments cause why not

--- Search.js
+++ Search.js
@@ -3,29 +3,34 @@ const traverse = function() {
   let indexOfCurrent = 0;
   let current = openList[0];
   for (let x = 0; x < openList.length; x++) {
-    if (openList[x].f < current.f) {
+    if (openList[x].h < current.h) {
       current = openList[x];
       indexOfCurrent = x;
     }
   }
 
+  // Exit if target reached
   if (current === target) {
     noLoop();
     return;
   }
 
-  // Traversing
+  // ==== Traversing
+  // Take out the current item and put it into the closed list (avoid inf loops)
   if (indexOfCurrent > -1) {
     openList.splice(indexOfCurrent, 1);
   }
 
   closedList.push(current);
+  // Generate all successors and traverse them
   current.addSuccessors(grid);
   current.successors.forEach(child => {
+    // Skip closed successors
     if (closedList.includes(child)) {
       return;
     }
 
+    // Calculate ghf
     child.g = current.g + current.getDistance(child);
     child.h = target.getDistance(child);
     child.f = child.g + child.h;
